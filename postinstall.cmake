96fcd0602a60a6675d9c3636d9dbc06be553a807
John R. Hoare <john.hoare@kitware.com>
2016-03-23 13:07:33 -0400

    Removed debug output and changed to multiple execute_process calls

--- postinstall.cmake
+++ postinstall.cmake
@@ -1,5 +1,4 @@
 if (APPLE)
-  message("JRH: Bullet postinstall.cmake running")
 	set(BULLET_INSTALL_LIBS
 		libBullet3OpenCL_clew.2.83.dylib
 		libBullet2FileLoader.2.83.dylib
@@ -15,12 +14,10 @@ if (APPLE)
 	foreach(__lib ${BULLET_INSTALL_LIBS})
 		set(fixup_deplib_command)
 		foreach(__deplib ${BULLET_INSTALL_LIBS})
-			list(APPEND fixup_deplib_command
+			execute_process(
 				COMMAND install_name_tool -change ${__deplib} ${CMAKE_INSTALL_PREFIX}/lib/${__deplib} ${CMAKE_INSTALL_PREFIX}/lib/${__lib} )
 		endforeach()
     execute_process(
-			COMMAND install_name_tool -id ${CMAKE_INSTALL_PREFIX}/lib/${__lib}  ${CMAKE_INSTALL_PREFIX}/lib/${__lib}
-			${fixup_deplib_command})
-    message("JRH: fixup_deplib_command: ${fixup_deplib_command}")
+			COMMAND install_name_tool -id ${CMAKE_INSTALL_PREFIX}/lib/${__lib}  ${CMAKE_INSTALL_PREFIX}/lib/${__lib})
 	endforeach()
 endif()
